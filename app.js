c02ed62da8f78bf374ac2410d45c3a25a662c742
pkwesst <pkwesst@gmail.com>
2022-04-12 03:28:56 +0900

    Login

--- app.js
+++ app.js
@@ -3,14 +3,29 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 const greeting = document.querySelector("#greeting");
 
-    const HIDDEN_CLASSNAME = "hidden";
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-    function onLoginSubmit(event) {
+function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    greeting.innerText = `Hello ${username}`; //==="Hello " + username;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-    }
+    localStorage.setItem(USERNAME_KEY, username);
+    painGreetings(username);
+}
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+function painGreetings(username){
+    greeting.innerHTML = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    painGreetings(savedUsername);
+}
