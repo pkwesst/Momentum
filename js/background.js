9dc793ab43c71bd4c9f94f747159da38073d1d21
pkwesst <pkwesst@gmail.com>
2022-04-12 15:06:01 +0900

    img size

--- js/background.js
+++ js/background.js
@@ -5,5 +5,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
+bgImage.classList = 'imgSize';
 
 document.body.appendChild(bgImage);
